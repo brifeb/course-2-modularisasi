6cdd2d8f0abddceba7e1967f1503e2a3a26c3ef5
brifeb <brifeb@gmail.com>
2021-02-15 14:49:13 +0700

    rekuest ke detik update

--- use_request.py
+++ use_request.py
@@ -1,11 +1,12 @@
 import requests
 
 url = 'https://detik.com'
+
 try:
     respon = requests.get(url)
     if respon.status_code == 200:
-        print(f'Success! {respon.status_code}')
-        print(f'Content: {respon.text}')
+        print(f'Success! {respon.status_code}')     // berhasil
+        print(f'Content: {respon.text}')            // cetak semua hasil
     else:
         print('Gagal')
 except Exception as e:
